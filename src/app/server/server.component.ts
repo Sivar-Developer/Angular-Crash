d8dac29bf58fd4b2684a6bbec74c4011c46e1169
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-09-09 16:22:15 +0300

    ngClass dynamic classes ng directive

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl:  './server.component.html'
+  templateUrl:  './server.component.html',
+  styles: [`
+    .online {
+      color: white;
+    }
+    .offline {
+      color: white;
+    }
+  `]
 })
 export class ServerComponent {
   serverId = 10;
@@ -17,6 +25,6 @@ export class ServerComponent {
   }
 
   getColor() {
-    return this.serverStatus === 'online' ? 'green' : 'red';
+    return this.serverStatus === 'online' ? 'darkgreen' : 'darkred';
   }
 }
