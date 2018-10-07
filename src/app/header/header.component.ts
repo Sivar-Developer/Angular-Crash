b1f361f431b9af7dd91869b5f345185687c9e1ec
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-10-08 01:53:32 +0300

    Create a logging service

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,13 @@ export class HeaderComponent {
 
   @Output() featureSelected = new EventEmitter<string>();
 
+
   onSelect(feature: string) {
     this.featureSelected.emit(feature);
+  }
+
+  onMain(accountStatus: string) {
+    const service = new LoggingService;
+    service.logStatusChange(accountStatus);
   }
 }
