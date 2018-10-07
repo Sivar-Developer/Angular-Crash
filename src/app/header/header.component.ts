4f6d303b731f5bce49944dbafe1f28b9327874f5
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-10-08 02:13:54 +0300

    Injecting logging service into components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,19 +3,20 @@ import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  providers: [LoggingService]
 })
 export class HeaderComponent {
 
   @Output() featureSelected = new EventEmitter<string>();
 
+  constructor(private loggingService: LoggingService) { }
 
   onSelect(feature: string) {
     this.featureSelected.emit(feature);
   }
 
   onMain(accountStatus: string) {
-    const service = new LoggingService;
-    service.logStatusChange(accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
