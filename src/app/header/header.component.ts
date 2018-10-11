8c3c7ae3a5c23a15885b58d51d17280b8d2a2f2a
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-10-12 01:20:29 +0300

    Using custom observable from scratch

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 // import { LoggingService } from '../logging.service';
-import { Observable } from 'rxjs/Observable';
-import { Observer } from 'rxjs/Observer';
-import { map, filter, switchMap } from 'rxjs/operators';
+// import { Observable, Observer } from 'rxjs';
+// import { map } from 'rxjs/operators';
+// import 'rxjs/Rx';
+// import { map, filter, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -11,31 +12,31 @@ import { map, filter, switchMap } from 'rxjs/operators';
 })
 export class HeaderComponent {
 
-  const myObservable = Observable.create((observer: Observer<string>) => {
+  // const myObservable = Observable.create((observer: Observer<string>) => {
 
-    setTimeout(() => {
-      observer.next('first package');
-    }, 2000);
+  //   setTimeout(() => {
+  //     observer.next('first package');
+  //   }, 2000);
 
-    setTimeout(() => {
-      observer.next('first package');
-    }, 2000);
+  //   setTimeout(() => {
+  //     observer.next('first package');
+  //   }, 2000);
 
-    setTimeout(() => {
-      observer.complete();
-    }, 2000);
+  //   setTimeout(() => {
+  //     observer.complete();
+  //   }, 2000);
 
-    setTimeout(() => {
-      observer.error('first package');
-    }, 2000);
+  //   setTimeout(() => {
+  //     observer.error('first package');
+  //   }, 2000);
 
-  });
+  // });
 
   // myObservable.subscribe(
   //   (data: string) => { console.log(data); },
   //   (error: string) => { console.log(error); },
-  //   () => { console.log('completed'); }
-  // );
+  //   () => { console.log('completed'); };
+  // )
 
 
 
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/observer';
+import 'rxjs/Rx';
+
 import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
 
@@ -22,6 +26,32 @@ export class RecipesComponent implements OnInit {
           this.selectedRecipe = recipe;
         }
       );
+
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000);
+
+      setTimeout(() => {
+        observer.next('second package');
+      }, 4000);
+
+      setTimeout(() => {
+        // observer.error('this does not work');
+        observer.complete();
+      }, 5000);
+
+      setTimeout(() => {
+        observer.next('third package');
+      }, 6000);
+
+    });
+
+    myObservable.subscribe(
+      (data: string) => { console.log(data); },
+      (error: string) => { console.log(error); },
+      () => { console.log('completed'); }
+    );
   }
 
 }
