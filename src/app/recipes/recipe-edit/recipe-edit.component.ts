f17856f60a49a60001d695bd384a9edce3991f77
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-10-14 00:34:18 +0300

    Syncing HTML with the form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -32,6 +32,10 @@ export class RecipeEditComponent implements OnInit {
       );
   }
 
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
   private initForm() {
     let recipeName = '';
     let recipeImagePath = '';
