d46a138082ed97efec0c9f4be9c7e38e6d301586
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-10-14 18:09:47 +0300

    Adding headers to firebase post request

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -9,6 +9,7 @@ export class ServerService {
   constructor(private http: Http) { }
 
   storeServers(servers: any) {
-    return this.http.post('https://angular-course-c8106.firebaseio.com/data.json', servers);
+    const headers = new Headers({'Content-Type': 'application/json'});
+    return this.http.post('https://angular-course-c8106.firebaseio.com/data.json', servers, { headers: headers });
   }
 }
